a9d9498e0380d3126c8b7d6836b3e41e95ec648b
dharmeshkakadia <dharmesh.kakadia@gmail.com>
2013-07-13 15:58:10 +0530

    Added hotel and review classes

--- src/food/service/Hotel.java
+++ src/food/service/Hotel.java
@@ -1,7 +1,48 @@
 package food.service;
 
+import java.util.ArrayList;
+
 public class Hotel {
 	int hotelId;
 	String hotelName;
-	
+	ArrayList<Item> menuItems;
+	double lat;
+	double lng;
+	ArrayList<Review> userReviews;
+	public int getHotelId() {
+		return hotelId;
+	}
+	public void setHotelId(int hotelId) {
+		this.hotelId = hotelId;
+	}
+	public String getHotelName() {
+		return hotelName;
+	}
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+	public ArrayList<Item> getMenuItems() {
+		return menuItems;
+	}
+	public void setMenuItems(ArrayList<Item> menuItems) {
+		this.menuItems = menuItems;
+	}
+	public double getLat() {
+		return lat;
+	}
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+	public double getLng() {
+		return lng;
+	}
+	public void setLng(double lng) {
+		this.lng = lng;
+	}
+	public ArrayList<Review> getUserReviews() {
+		return userReviews;
+	}
+	public void setUserReviews(ArrayList<Review> userReviews) {
+		this.userReviews = userReviews;
+	}
 }
---
+++ src/food/service/Review.java
@@ -0,0 +1,43 @@
+package food.service;
+
+import java.util.Date;
+
+public class Review {
+	int reviewId;
+	String authorName;
+	String desc;
+	int rating;
+	Date date;
+	public int getReviewId() {
+		return reviewId;
+	}
+	public void setReviewId(int reviewId) {
+		this.reviewId = reviewId;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+}
